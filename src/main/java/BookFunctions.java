e87583b1857437d0363fa70e8c0496a30a46e0e2
Tralphi <rtarnawski@gmail.com>
2018-08-12 15:42:56 +0200

    some more methot using tdd

--- src/main/java/BookFunctions.java
+++ src/main/java/BookFunctions.java
@@ -1,3 +1,7 @@
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BookFunctions {
@@ -6,14 +10,95 @@ public class BookFunctions {
 
     public static Book findBookByISBN(List<Book> bookList, int isbn){
 
-        Book findedBook = null;
-
-        for (Book bookInLoop : bookList) {
+            for (Book bookInLoop : bookList) {
             if (isbn == bookInLoop.getIsbn()){
-                findedBook = bookInLoop;
+                return bookInLoop;
             }
         }
-        return findedBook;
+        return null;
+    }
+
+public static long sumOfYearsOfPublishingDatesForAllBooks(List<Book> bookList){
+        long sum = 0;
+    for (Book bookInLoop : bookList) {
+        sum += bookInLoop.getPublishingDate();
+    }
+        return sum;
+}
+
+public static int sumOfBooksPublishedAfterYear(List<Book>bookList, int year){
+
+        int sum = 0;
+    for (Book bookInLoop : bookList) {
+        if (bookInLoop.getPublishingDate() > year){
+            sum+=1;
+        }
+    }
+        return sum;
+}
+
+public static boolean areAllPublishedAfterYear(List<Book> bookList, int year){
+
+    for (Book bookInLoop : bookList) {
+        if (bookInLoop.getPublishingDate() <= year)
+        return false;
+    }
+        return true;
+}
+
+public static List<Book> getTwoLastBooks(List<Book> bookList){
+        List<Book> twoLastBooks = new ArrayList<>();
+
+    for (int i = bookList.size()-2; i < bookList.size(); i++) {
+        twoLastBooks.add(bookList.get(i));
+    }
+    return twoLastBooks;
+
+}
+
+
+public static List<Book> getTheNewestBook(List<Book> bookList){
+
+        List<Book> theNewestBooks = new ArrayList<>();
+
+        int highYear = 0;
+
+    for (Book bookInLoop : bookList) {
+
+        if (bookInLoop.getPublishingDate() > highYear){
+            theNewestBooks.clear();
+            theNewestBooks.add(bookInLoop);
+
+            highYear = bookInLoop.getPublishingDate();
+        } else if(bookInLoop.getPublishingDate() == highYear){
+            theNewestBooks.add(bookInLoop);
+        }
+
+    }
+return theNewestBooks;
+
+}
+
+    public static List<Book> getTheOldestBook(List<Book> bookList){
+
+        List<Book> theOldestBooks = new ArrayList<>();
+
+        int lowYear = Integer.MAX_VALUE;
+
+        for (Book bookInLoop : bookList) {
+
+            if (bookInLoop.getPublishingDate() < lowYear){
+                theOldestBooks.clear();
+                theOldestBooks.add(bookInLoop);
+
+                lowYear = bookInLoop.getPublishingDate();
+            } else if(bookInLoop.getPublishingDate() == lowYear){
+                theOldestBooks.add(bookInLoop);
+            }
+
+        }
+        return theOldestBooks;
+
     }
 
 }
--- src/test/java/BookFunctionsTest.java
+++ src/test/java/BookFunctionsTest.java
@@ -1,45 +1,78 @@
+import com.google.common.collect.Lists;
 import org.junit.Assert;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class BookFunctionsTest {
-
-
-
+public class BookFunctionsTest {
     List<Book> testBookList = new ArrayList<>();
+    BooksCategory testBooksCategory = new BooksCategory(1,"Science", 1);
+    List<Author> testAuthorsList = Lists.newArrayList(new Author(1,"Martha", "Jane", 38));
 
-    @BeforeAll
-  void start(){
-
-        List<Author> testAuthorsList = new ArrayList<>();
-        testAuthorsList.add(new Author(1,"Martha", "Jane", 38) );
-
-        BooksCategory testBooksCategory = new BooksCategory(1,"Science", 1);
-
+    @Before
+    public void start(){
 
         testBookList.add(new Book(1, "core java", 15847, 2017, BookBinding.M, testAuthorsList,testBooksCategory ));
         testBookList.add(new Book(2, "core php", 16582, 2015, BookBinding.T, testAuthorsList,testBooksCategory ));
         testBookList.add(new Book(3, "core html", 15886, 2007, BookBinding.M, testAuthorsList,testBooksCategory ));
-        testBookList.add(new Book(4, "core c++", 165582, 2005, BookBinding.T, testAuthorsList,testBooksCategory ));
+        testBookList.add(new Book(4, "core c++", 165582, 2018, BookBinding.T, testAuthorsList,testBooksCategory ));
         testBookList.add(new Book(5, "core python", 1869558, 2018, BookBinding.M, testAuthorsList,testBooksCategory ));
+    }
+
+    @Test
+    public void findBookByISBNShouldReturnProperBook() {
+        int isbn = 15886;
+        Book bookByISBN = BookFunctions.findBookByISBN(testBookList, isbn);
+        Assert.assertEquals(isbn, bookByISBN.getIsbn());
+    }
+
+    @Test
+    public void sumOfYearsOfPublishingDatesForAllBooks() {
+
+        Assert.assertEquals(10075,BookFunctions.sumOfYearsOfPublishingDatesForAllBooks(testBookList) );
+    }
+
+    @Test
+    public void sumOfBooksPublishedAfterYear() {
+
+        Assert.assertEquals(4,BookFunctions.sumOfBooksPublishedAfterYear(testBookList, 2007));
     }
 
 
     @Test
-    void findBookByISBNShouldReturnProperBook() {
-        int isbn = 15886;
+    public void areAllPublishedAfterYear() {
 
-        Book bookByISBN = BookFunctions.findBookByISBN(testBookList, isbn);
+        Assert.assertTrue(BookFunctions.areAllPublishedAfterYear(testBookList, 2000));
+    }
 
-        Assert.assertEquals(isbn, bookByISBN.getIsbn());
+    @Test
+    public void getTwoLastBooks() {
+
+
+
+        Assert.assertEquals(2, BookFunctions.getTwoLastBooks(testBookList).size());
+        Assert.assertEquals(165582, BookFunctions.getTwoLastBooks(testBookList).get(0).getIsbn());
+        Assert.assertEquals(1869558, BookFunctions.getTwoLastBooks(testBookList).get(1).getIsbn());
+
+    }
+
+    @Test
+    public void getTheNewestBook() {
+
+        Assert.assertEquals(2, BookFunctions.getTheNewestBook(testBookList).size());
+        Assert.assertEquals(165582, BookFunctions.getTheNewestBook(testBookList).get(0).getIsbn());
+        Assert.assertEquals(1869558, BookFunctions.getTheNewestBook(testBookList).get(1).getIsbn());
+
+        }
+
+    @Test
+    public void getTheOldestBook() {
+        Assert.assertEquals(1, BookFunctions.getTheOldestBook(testBookList).size());
+        Assert.assertEquals(15886, BookFunctions.getTheOldestBook(testBookList).get(0).getIsbn());
 
     }
 }
